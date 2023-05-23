3f0bcb6d8de371861df1c0fdcac888fc0e5ff24e
Frances Leigh <fleighbaay@gmail.com>
2023-05-23 21:07:24 +0800

    Pseudocode

---
+++ Calculator_ui.py
@@ -0,0 +1,25 @@
+# Import modules for UI
+
+# Define CalculatorUI class to manage and customize the interface
+    # Ensure
+
+operations = ["+", "-", "*", "/"]
+
+# Create labels for operation and operation options 
+
+# Create labels and fields for numbers
+
+# Create label for result
+
+# Create calculate button
+
+# Define calculate method
+
+# Get the selected operation and the numbers
+
+# Call the calculate method from the CalculatorLogic class to perform calculation
+
+# Display error message in result label
+
+# Clear operation and number entry fields
+

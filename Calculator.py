9adc2e3a8442d66def549e3b79a5c54e0b04368e
Frances Leigh <fleighbaay@gmail.com>
2023-05-23 20:39:09 +0800

    Operations Algorithm

--- Calculator.py
+++ Calculator.py
@@ -1,8 +1,20 @@
 # Define class to import later
 
-    # Addition
-    # Subtraction
-    # Multiplication
-    # Division
-
+class Calculator:
+    def calculate(operation, num1, num2):
+        try:
+            # Addition
+            if operation == '+':
+                result = num1 + num2
+            # Subtraction
+            elif operation == '-':
+                result = num1 - num2
+            # Multiplication
+            elif operation == '*':
+                result == num1 * num2
+            # Division
+            elif operation == '/':
+                result = num1 / num2
 #Handle exceptions
+            else:
+            

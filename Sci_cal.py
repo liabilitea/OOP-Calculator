a19b87fea8d5729fdfa25d42cab71a01c0aa02e3
Frances Leigh <fleighbaay@gmail.com>
2023-06-03 16:09:07 +0800

    Exponent Operation

--- Sci_cal.py
+++ Sci_cal.py
@@ -3,8 +3,10 @@ import math
 from Calculator import Calculator
 
 # Create new class 
-
-# Create method to calculate using exponent operation
-
+class ScientificCalculator(Calculator):
+    # Create method to calculate using exponent operation
+    def power(self, num1, num2):
+        return num1 ** num2
+    
 # Create method t calculate using log operation
     # Use base 10 for log

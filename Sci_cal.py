8c7687abb1336910519fbeb89178b84bc245ada3
Frances Leigh <fleighbaay@gmail.com>
2023-06-03 16:05:21 +0800

    Pseudocode

---
+++ Sci_cal.py
@@ -0,0 +1,8 @@
+# Import necessary modules
+
+# Create new class 
+
+# Create method to calculate using exponent operation
+
+# Create method t calculate using log operation
+    # Use base 10 for log
